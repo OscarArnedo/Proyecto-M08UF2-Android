f3b614e23503087f785c9ff23b14bd76924f2a21
OscarArnedo <oscar.arnedo.riccobene@gmail.com>
2021-02-28 19:05:51 +0100

    Logo ruleta

--- app/src/main/java/com/example/proyectom08uf2android/MainActivity.java
+++ app/src/main/java/com/example/proyectom08uf2android/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.proyectom08uf2android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        Intent intent = new Intent(getApplicationContext(),
+                GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void ranking(View view) {
+    }
+
+    public void exit(View view) {
     }
 }
