51b7330858e9f1f57a39f5b630c1fcf8ae8e84a4
Oscar Arnedo <62158472+OscarArnedo@users.noreply.github.com>
2021-03-02 18:46:29 +0100

    Musica, funciona pero a lo cutre.

--- app/src/main/java/com/example/proyectom08uf2android/MainActivity.java
+++ app/src/main/java/com/example/proyectom08uf2android/MainActivity.java
@@ -2,16 +2,46 @@ package com.example.proyectom08uf2android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toolbar;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isPlaying = true;
+    private Intent intent;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*intent = new Intent(this, AudioIntentService.class);
+        intent.putExtra("operacio", "inici");
+        startService(intent);*/
+
+        mp = MediaPlayer.create(this, R.raw.musica_casino);
+        mp.start();
+
+    }
+
+
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //Función para añadir el menú definido en menu_principal.xml al action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.layout.menu_principal, menu);
+        return true;
     }
 
     public void play(View view) {
@@ -25,4 +55,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void exit(View view) {
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.playStopMusic:
+                String text;
+
+                if (!isPlaying) {
+                    isPlaying = !isPlaying;
+                    text = "PAUSE MUSIC";
+                    item.setTitle(text);
+                    mp.start();
+                    //intent.putExtra("operacio", "inici");
+                    //startService(intent);
+                } else {
+                    isPlaying = !isPlaying;
+                    text = "PLAY MUSIC";
+                    item.setTitle(text);
+                    mp.pause();
+                    //intent.putExtra("operacio", "pausa");
+                    //startService(intent);
+                }
+
+                return true;
+            /*case R.id.:
+                exportar();
+                return true;*/
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
+
