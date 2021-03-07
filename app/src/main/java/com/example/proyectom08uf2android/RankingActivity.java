2219d5ce41fc6d0e65998b224ecfa60183fd4f95
Oscar Arnedo <62158472+OscarArnedo@users.noreply.github.com>
2021-03-07 20:14:56 +0100

    RankingActivity

---
+++ app/src/main/java/com/example/proyectom08uf2android/RankingActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyectom08uf2android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+
+
+    }
+}
